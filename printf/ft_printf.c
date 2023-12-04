167a044a45052087bd4dcabead9a7c42d5e85b12
jollagit <143734619+jollagit@users.noreply.github.com>
2023-12-04 19:52:08 +0100

    Add files via upload

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,39 @@
 /*   By: giuliaviga <giuliaviga@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 15:07:02 by gvigano           #+#    #+#             */
-/*   Updated: 2023/12/04 19:29:59 by giuliaviga       ###   ########.fr       */
+/*   Updated: 2023/12/04 19:44:19 by giuliaviga       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+static int	ft_print_char(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+static int	ft_search_format(va_list ptr, const char s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == 'c')
+		i += ft_print_char(va_arg(ptr, int));
+	else if (s == 's')
+		i += ft_print_str(va_arg(ptr, char *));
+	else if (s == 'p')
+		i += ft_print_ptr(va_arg(ptr, unsigned long long));
+	else if (s == 'd' || s == 'i')
+		i += ft_print_nbr(va_arg(ptr, int));
+	else if (s == 'u')
+		i += ft_print_unbr(va_arg(ptr, unsigned int));
+	else if (s == 'x' || s == 'X')
+		i += ft_print_hex(va_arg(ptr, unsigned int), s);
+	else if (s == '%')
+		i += write(1, '%', 1);
+	return (i);
+}
 
 int	ft_printf(const char *str, ...)
 {
--- printf/printf.h
+++ printf/printf.h
@@ -9,13 +9,10 @@
 # include <stdint.h>
 
 int	ft_print_nbr(int n);
-int	ft_print_char(char c);
 int	ft_print_str(char *s);
-int	ft_print_percent(void);
 int	ft_print_ptr(const void *ptr);
 int	ft_print_unbr(unsigned int n);
 int	ft_printf(const char *str, ...);
 int	ft_print_hex(unsigned int n, const char c);
-int	ft_search_format(va_list ptr, const char s);
 
 #endif
